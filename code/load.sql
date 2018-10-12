8cf8aee9f549ba0a1ec521a3780f1afa8c9ba87a
Neil Luo <nl123@duke.edu>
2018-10-11 22:31:38 -0400

    update README and load.sql

---
+++ code/load.sql
@@ -0,0 +1,46 @@
+INSERT INTO Player VALUES
+  (1000000001, 'Andrew Cohen', 'Atlanta Hustle'),
+  (1000000002, 'Ben Dieter', 'Los Angeles Aviators'),
+  (1000000003, 'CJ Colicchio', 'Chicago Wildfire'),
+  (1000000004, 'Justin Allen', 'DC Breeze'),
+  (1000000005, 'Matt Ellis', 'Madison Radicals'),
+  (1000000006, 'Jacob Fairfax', 'New York Empire');
+
+INSERT INTO FantasyTeam VALUES
+  (1000000001, 'Alpha', 3, 0),
+  (2000000002, 'Bravo', 2, 1),
+  (3000000003, 'Charlie', 1, 0),
+  (4000000004, 'Delta', 0, 2),
+  (5000000005, 'Echo', 0, 1),
+  (6000000006, 'Foxtrot', 1, 2),
+  (7000000007, 'Golf', 0, 1);
+
+INSERT INTO Owner VALUES
+  (1000000001, 'username', 'password'),
+  (2000000002, 'abc', '123');
+
+INSERT INTO Game VALUES
+  (100000001, 1),
+  (200000002, 2),
+  (300000003, 3),
+  (400000004, 4),
+  (500000005, 5);
+
+INSERT INTO FantasyGame VALUES
+  (100000001, 1),
+  (200000002, 1),
+  (300000003, 1),
+  (400000004, 2),
+  (500000005, 2),
+  (600000006, 3),
+  (700000007, 3);
+
+INSERT INTO PlayedIn VALUES
+INSERT INTO FantasyPlayedIn VALUES
+INSERT INTO IsOn VALUES
+INSERT INTO Owns VALUES
+
+
+
+
+
