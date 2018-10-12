f2c330b8ea63212358bdca3e430726ef95f21d68
Neil Luo <nl123@duke.edu>
2018-10-11 22:40:49 -0400

    finish initial load.sql and README.md

--- code/load.sql
+++ code/load.sql
@@ -1,10 +1,10 @@
 INSERT INTO Player VALUES
   (1000000001, 'Andrew Cohen', 'Atlanta Hustle'),
-  (1000000002, 'Ben Dieter', 'Los Angeles Aviators'),
-  (1000000003, 'CJ Colicchio', 'Chicago Wildfire'),
-  (1000000004, 'Justin Allen', 'DC Breeze'),
-  (1000000005, 'Matt Ellis', 'Madison Radicals'),
-  (1000000006, 'Jacob Fairfax', 'New York Empire');
+  (2000000002, 'Ben Dieter', 'Los Angeles Aviators'),
+  (3000000003, 'CJ Colicchio', 'Chicago Wildfire'),
+  (4000000004, 'Justin Allen', 'DC Breeze'),
+  (5000000005, 'Matt Ellis', 'Madison Radicals'),
+  (6000000006, 'Jacob Fairfax', 'New York Empire');
 
 INSERT INTO FantasyTeam VALUES
   (1000000001, 'Alpha', 3, 0),
@@ -20,25 +20,34 @@ INSERT INTO Owner VALUES
   (2000000002, 'abc', '123');
 
 INSERT INTO Game VALUES
-  (100000001, 1),
-  (200000002, 2),
-  (300000003, 3),
-  (400000004, 4),
-  (500000005, 5);
+  (1000000001, 1),
+  (2000000002, 2),
+  (3000000003, 3),
+  (4000000004, 4),
+  (5000000005, 5);
 
 INSERT INTO FantasyGame VALUES
-  (100000001, 1),
-  (200000002, 1),
-  (300000003, 1),
-  (400000004, 2),
-  (500000005, 2),
-  (600000006, 3),
-  (700000007, 3);
+  (1000000001, 1),
+  (2000000002, 1),
+  (3000000003, 1),
+  (4000000004, 2),
+  (5000000005, 2),
+  (6000000006, 3),
+  (7000000007, 3);
 
 INSERT INTO PlayedIn VALUES
+  (1000000001, 2000000002, 1, 6, 2, 2, 3, 1);
 INSERT INTO FantasyPlayedIn VALUES
+  (1000000001, 3000000003, TRUE, '{4,3}');
 INSERT INTO IsOn VALUES
+  (1000000001, 1000000001),
+  (2000000002, 1000000001),
+  (3000000003, 1000000001),
+  (4000000004, 1000000001),
+  (5000000005, 1000000001);
 INSERT INTO Owns VALUES
+  (1000000001, 1000000001),
+  (2000000002, 2000000002);
 
 
 
