ad5273b155dc89b24f6d66ea02c82c08195afcaa
Edward Zhuang <edward.zhuang@duke.edu>
2018-11-08 13:10:39 -0500

    connected to postgreSQL

--- app/models.py
+++ app/models.py
@@ -1,2 +1,17 @@
 from app import db
 
+
+# class User(db.Model):
+#     AGENT_CODE = db.Column(db.String(6), primary_key=True)
+#     AGENT_NAME = db.Column(db.String(40))
+#     WORKING_AREA = db.Column(db.String(35))
+#     COMMISSION = db.Column(db.Float(10))
+#     PHONE_NO = db.Column(db.String(15))
+#     COUNTRY = db.Column(db.String(25))
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    teamname = db.Column(db.String(64), index=True)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
--- config.py
+++ config.py
@@ -1,8 +1,37 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+POSTGRES = {
+    'user': 'postgres',
+    'pw': 'qwerty',
+    'db': 'datastone',
+    'host': 'localhost',
+    'port': '5432',
+}
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'datastone-bois'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-                              'sqlite:///' + os.path.join(basedir, 'app.db')
+    # SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = 'postgresql://%(user)s:\
+%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG = False
+    CSRF_ENABLED = True
+    TESTING = False
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
